176a998d1fd53f3dde593034c5186e8256e2f30c
thucvinguyen <thucvinguyen@gmail.com>
2024-03-04 10:02:51 +0700

    commit msg

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import MovieCard from "./MovieCard";
-import { Grid } from "@mui/material";
+import { Grid, Pagination } from "@mui/material";
 
 function MovieList({ movies }) {
+  const [page, setPage] = useState(1);
+  const itemsPerPage = 8;
+  const startIndex = (page - 1) * itemsPerPage;
+  const endIndex = page * itemsPerPage;
+  const currentMovies = movies.slice(startIndex, endIndex);
+  const handlePageChange = (event, value) => {
+    setPage(value);
+  };
+
   return (
-    <Grid container spacing={3}>
-      {movies.map((movie, index) => (
-        <Grid item key={movie.movieId} xs={12} md={6} lg={3}>
-          <MovieCard movie={movie} />
-        </Grid>
-      ))}
-    </Grid>
+    <>
+      <Grid container spacing={3}>
+        {currentMovies.map((movie, index) => (
+          <Grid item key={movie.movieId} xs={12} md={6} lg={3}>
+            <MovieCard movie={movie} />
+          </Grid>
+        ))}
+      </Grid>
+      <Pagination
+        count={Math.ceil(movies.length / itemsPerPage)}
+        page={page}
+        onChange={handlePageChange}
+        sx={{ mt: 3, display: "flex", justifyContent: "center" }}
+      />
+    </>
   );
 }
 
